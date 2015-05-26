e84c1147df6f36c5685cf55c5de67a666fe94ceb
Emily Tucker <emily.tucker@western.edu>
2015-05-26 14:15:59 -0600

    fixed the carousel items

--- js/constructors.js
+++ js/constructors.js
@@ -118,3 +118,4 @@ function Image(name, fname) {
 }
 
 
+
--- js/menu.js
+++ js/menu.js
@@ -40,3 +40,62 @@ function dispMainMenu() {
     $("#drilldown-1").html(inner);
 }
 
+/***********************
+     *  Carousel Items generator   *
+	     ***********************/
+
+ $(document).ready(function(){
+    $("#hide").click(function(){
+		/*alert("Clicked")*/
+			if ( display === true ) {
+		  $( "#carousel" ).show();
+		} else if ( display === false ) {
+		  $( "#carousel" ).hide();
+		}
+    });
+});
+		 
+var ciCount = 0;
+
+var carouselItems = [];
+var thumbs = [];
+
+function getCIs(tag) /*Carousel Items*/{
+    caouselItems = [];
+    thumbs = [];
+    var inner, ci, i, j, a;
+    ciCount = 0;
+    inner = "";
+    for(i in CIs) {
+        ci = CIs[i];
+        for (j in ci.tags) {
+            if (ci.tags[j] === tag) {
+                //build carousel item
+                a = "<div class='citem' id='citem" + ciCount + "'>";
+                if (ci.video) {
+                    a += ci.html;
+                } else {
+                    a += "<a class='fancybox' data-fancybox-group='gallery' href = '"+ci.full.fname+"' title='"+ci.html+"' rel='gallery'>";
+                }
+                a += "<div class='Thumbimage' id='thumbImage" + ciCount + "'>";
+                a += "<img id='image 'src='"+ci.thumb.fname+"' alt=''>";
+                a += "<div class='Thumbcaption'>";
+                a += "<p>'"+ci.ttip+"'</p>";
+                a += "</div></div></a></div>";
+                inner += a;
+                carouselItems.push('citem' + String(ciCount));
+                thumbs.push('thumbImage' + String(ciCount));
+                ciCount += 1;
+                break;
+            }
+        }
+    }
+    width = window.innerWidth - (ciCount * 160);
+    margin = width / (ciCount-1) * .75;
+	
+    $("#carousel").html(inner);
+    $(".citem").css("margin-left", margin);
+
+    return carouselItems;
+}
+
