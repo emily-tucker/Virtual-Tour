f9155b1619012f859a9d0696714e9cc1b571552d
Emily Tucker <emily.tucker@western.edu>
2015-05-28 13:51:42 -0600

    Chnages

--- js/main.js
+++ js/main.js
@@ -44,12 +44,19 @@ var map_button_in_time = 750;
 var first_time = false;
 var is_mobile = false;
 var tour_track = 0;
-
+var showHide = true; /*bool to determine weather to show or hide the carousel.
 var currentLocation; //used by just about everything, initialized here
 var previousLocation = locations[0]; //used for off campus/on campus switch
+/********************************
+*  Test for Mobility            *
+*********************************/
+if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))){
 
-
-
+ is_mobile = true;
+ map_state = 0;
+ /*alert("Mobile Device Detected: "+is_mobile+map_state);*/
+}
 /********************************
  *  Map Animation Function for  *
  *  Current Location            *
@@ -128,24 +135,24 @@ function getNavs(locationTag) {
 	console.log(tour_track);
     $('.tipsy:last').remove();
 	 if (currentLocation.locationType === "default") {
-        var inner_html = "<img onclick=javascript:window.location.hash='#fieldhouse' class='to_athletics " +
-            "arrow' src='imgs/athTour.png' " +
-            "onmouseover=this.src='imgs/athTour.png'" +
-            " onmouseout=this.src='imgs/athTour_hover.png' " +
-            "title = '' />" +
+        var inner_html = "<img onclick=javascript:window.location.hash='#begin' class='to_begin " +
+            "arrow' src='imgs/right_slate.png' " +
+            "onmouseover=this.src='imgs/nav_arrows/right_offcampus_hover.png'" +
+            " onmouseout=this.src='imgs/nav_arrows/right_offcampus.png " +
+            "title = 'to Athletics' />" +
             "<img onclick=javascript:window.location.hash='#library' class='to_studentlife " +
             "arrow' src='imgs/nav_arrows/left_crimson.png' " +
             "onmouseover=this.src='imgs/nav_arrows/left_newtype_hover.png'" +
             " onmouseout=this.src='imgs/nav_arrows/left_crimson.png' " +
             "title = 'to the Default tour' />";
 	 }
-    
+    /*
     if (currentLocation.locationType === "academic" || currentLocation.locationType === "walkway") {
         var inner_html = "<img onclick=javascript:window.location.hash='#fieldhouse' class='to_athletics " +
-            "arrow' src='imgs/athTour.png' " +
-            "onmouseover=this.src='imgs/athTour.png'" +
-            " onmouseout=this.src='imgs/athTour_hover.png' " +
-            "title = '' />" +
+            "arrow' src='imgs/right_slate2.png' " +
+            "onmouseover=this.src='imgs/right_newtype_hover2.png'" +
+            " onmouseout=this.src='imgs/right_slate2.png' " +
+            "title = 'to Athletics' />" +
             "<img onclick=javascript:window.location.hash='#library' class='to_studentlife " +
             "arrow' src='imgs/left_crimson.png' " +
             "onmouseover=this.src='imgs/left_newtype_hover.png'" +
@@ -176,7 +183,7 @@ function getNavs(locationTag) {
             " onmouseout=this.src='imgs/right_whiteSL.png' " +
             "title = 'to Academics' />";
     }
-    
+    */
     var items = [];
     for (var i in navs) {
         if (currentLocation.onCampus) {
@@ -578,16 +585,13 @@ function detectIE() {
             
             /**Functions that shows or hides Carousel based on mobility**/
 
-var is_mobile=false;
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
- is_mobile=true;
-}
-var showHide = true;
+
 if(is_mobile){
-    showHide = false;
-}else{
-    /*alert("Showhide: "+showHide);*/
-}
+    document.getElementById('carousel').style.display = 'none'; /* hides carousel at first on mobile devices*/
+    showHide = true;
+}/*else{
+    alert("Showhide: "+showHide);
+}*/
 
  $(document).ready(function(){
     $("#hide").click(function(){
