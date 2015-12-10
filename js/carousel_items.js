83f5032aac3986a520bcd68f370d4b7f769b0779
Emily Tucker <emily.tucker@western.edu>
2015-12-10 16:55:02 -0700

    Testing a stylesheet for landscape

--- js/carousel_items.js
+++ js/carousel_items.js
@@ -1,36 +1,82 @@
 
 //Taylor___________________________________________________________
 
-var taylor_1full = new Image("Taylor_", "imgs/taylor_1full.jpg");
-var Taylor_STZoeSmithThumb = new Image("Taylor_STZoeSmithThumb", "imgs/Taylor_1thumb.jpg");
-var Taylor_STZoeSmith = new CarouselItem(["#taylor"], Taylor_STZoeSmithFull, Taylor_STZoeSmithThumb, " Zoe smith testimonial ", '<b>Student Testimonials - Zoe Smith</b><b>Where are you from?</b> <br />I’m from Rockford, Illinois about 60 miles outside of Chicago.<b>Why did you come to Western?</b> <br />I came to Western because I came for a tour and was looking at schools all around the intermountain west, and then I came backto Western for a preview day. They had one of the old buildings sprayed with ice to form a climbing wall. Through the entire time, my admissions counselor was in really close contact with me and I felt like if I went to Western my success really mattered and they were committed to helping me out. It was the best decision I ever made!<br /></br>&lt;a target="_self" onClick=&quot;window.location.href=&#x27;http://www.western.edu/future-students/student-testimonials-and-profiles&#x27;&quot;href="http://www.western.edu/future-students/student-testimonials-and-profiles"&gt;Click here to read more...&lt;/a&gt;');
+var taylor_1full = new Image("taylor_1full", "imgs/taylor_1full.jpg");
+var taylor_1thumb = new Image("taylor_1thumb", "imgs/Taylor_1thumb.jpg");
+var taylor_1 = new CarouselItem(["#taylor"], taylor_1full, taylor_1thumb, "Classrooms in Taylor", 'Taylor Hall, the oldest building on campus, was fully renovated in 2010. It houses a number of smart classrooms that are utilized primarily by the Department of Communication Arts, Languages & Literature.</br>');
 
 
-var taylor_2full = new Image("Taylor_WildPursuitsFull", "imgs/taylor_2full.jpg");
-var Taylor_WildPursuitsThumb = new Image("Taylor_WildPursuitsThumb", "imgs/taylor_2thumb.jpg");
-var Taylor_WildPursuits = new CarouselItem(["#taylor"], Taylor_WildPursuitsFull, Taylor_WildPursuitsThumb, "Wilderness Persuits",  '<b>Wilderness Persuits Club</b><br />Wilderness Pursuits provides fun, low-cost, and professional-quality co-curricular outdoor expeditions and resources for the students of Western State Colorado University. Wilderness Pursuits trips are led by students and designed to meet the needs of a broad range of abilities and interests, while developing outdoor skills and fostering community, citizenry, environmental stewardship and healthy living.</br>&lt;a target="_self" onClick=&quot;window.location.href=&#x27;http://www.western.edu/current-students/wilderness-pursuits/wp-2014-15-trips&#x27;&quot;   href="http://www.western.edu/current-students/wilderness-pursuits/wp-2014-15-trips"&gt;Click here to read more...&lt;/a&gt;');
+var taylor_2full = new Image("taylor_2full", "imgs/taylor_2full.jpg");
+var taylor_2thumb = new Image("taylor_2thumb", "imgs/taylor_2thumb.jpg");
+var taylor_2 = new CarouselItem(["#taylor"], taylor_2full, taylor_2thumb, "KWSB Student Radio",  'Founded in 1968, KWSB is Western’s non-commercial, student-run radio station. Housed in Taylor Hall, the radio station and studio space underwent complete renovations in 2010. With a little training, students can become live, on-air DJs.</br>&lt;a target="_self" onClick=&quot;window.location.href=&#x27;http://www.western.edu/kwsb&#x27;&quot;   href="http://www.western.edu/kwsb"&gt;Click here to read more...&lt;/a&gt;');
 
 
-var taylor_3full = new Image("Taylor_STChrisNoahFull", "imgs/taylor_3full.jpg");
-var taylor_3thumb = new Image("Taylor_STChrisNoahThumb", "imgs/taylor_3thumb.jpg");
-var Taylor_STChrisNoah = new CarouselItem(["#taylor"], Taylor_STChrisNoahFull, taylor_3thumb, 'Chris Noah testimonial', '<b>Student Testimonials - Chris Noah</b>Chris is working on a Recreation and Outdoor Education major with an emphasis in instruction. Wilderness Pursuits allows him to practice his instruction techniques while guiding rock, ice, and snow based trips for other students. </br>&lt;a target="_self" onClick=&quot;window.location.href=&#x27;http://www.western.edu/profile/student/chris-noah&#x27;&quot;   href="http://www.western.edu/profile/student/chris-noah"&gt;Click here to read more...&lt;/a&gt;');
+var taylor_3full = new Image("taylor_3full", "imgs/taylor_3full.jpg");
+var taylor_3thumb = new Image("taylor_3thumb", "imgs/taylor_3thumb.jpg");
+var taylor_3 = new CarouselItem(["#taylor"], taylor_3full, taylor_3thumb, 'The Taylor Auditorium', 'Taylor Hall’s Studio Theater offers an intimate acting space where the audience surrounds three sides of the stage. Peak Productions, Western’s co-curricular program and student-run production board, utilizes the space to present three to four quality live performances each year.');
 
-var taylor_4full = new Image("Taylor_STBradArcherFull", "imgs/taylor_4full.jpg");
-var taylor_4thumb = new Image("Taylor_STBradArcherThumb", "imgs/taylor_4thumb.jpg");
-var Taylor_STBradArcher = new CarouselItem(["#taylor"], Taylor_STBradArcherFull, taylor_4thumb, "Brad Archer testimonial", '<b>Student Testimonials - Brad Archer</b><br />By immersing himself in the community that is Western State Colorado University, Brad Archer says, he found direction for his life.He says he came to Western from his home of Milliken, Colo., to indulge his fascination with people. He had been a state officer in the Family, Career and Community Leaders of America, as well as an active citizen in the Potawatomi Nation of American Indians.</br>&lt;a target="_self" onClick=&quot;window.location.href=&#x27;http://www.western.edu/future-students/student-testimonials-and-profiles&#x27;&quot;   href="http://www.western.edu/future-students/student-testimonials-and-profiles"&gt;Click here to read more...&lt;/a&gt;');
+var taylor_4full = new Image("taylor_4", "imgs/taylor_4full.jpg");
+var taylor_4thumb = new Image("taylor_4thumb", "imgs/taylor_4thumb.jpg");
+var taylor_4 = new CarouselItem(["#taylor"], taylor_4full, taylor_4thumb, "Western's Welcome Center", 'Taylor Hall’s atrium houses the campus Welcome Center, where visitors can learn more about academic programs and campus life, meet student ambassadors and embark on campus tours.');
 
-var taylor_5full = new Image("Taylor_STBradArcherFull", "imgs/taylor_5full.jpg");
-var taylor_5thumb = new Image("Taylor_STBradArcherThumb", "imgs/taylor_5thumb.jpg");
-var Taylor_STBadArcher = new CarouselItem(["#taylor"], Taylor_STBadArcherFull, taylor_5thumb, "Brad Archer testimonial", '<b>Student Testimonials - Brad Archer</b><br />By immersing himself in the community that is Western State Colorado University, Brad Archer says, he found direction for his life.He says he came to Western from his home of Milliken, Colo., to indulge his fascination with people. He had been a state officer in the Family, Career and Community Leaders of America, as well as an active citizen in the Potawatomi Nation of American Indians.</br>&lt;a target="_self" onClick=&quot;window.location.href=&#x27;http://www.western.edu/future-students/student-testimonials-and-profiles&#x27;&quot;   href="http://www.western.edu/future-students/student-testimonials-and-profiles"&gt;Click here to read more...&lt;/a&gt;');
+var taylor_5full = new Image("taylor_5full", "imgs/taylor_5full.jpg");
+var taylor_5thumb = new Image("taylor_5thumb", "imgs/taylor_5thumb.jpg");
+var taylor_5 = new CarouselItem(["#taylor"], taylor_5full, taylor_5thumb, "Explore the Welcome Center", '<b>Taylor Hall’s atrium houses the campus Welcome Center, where visitors can learn more about academic programs and campus life, meet student ambassadors and embark on campus tours.');
+
+//Fieldhouse___________________________________________________________
+
+var fieldhouse_athletic_1full = new Image("fieldhouse_athletic_2full", "imgs/fieldhouse_athletic_1full.jpg");
+var fieldhouse_athletic_1thumb = new Image("fieldhouse_athletic_1thumb", "imgs/fieldhouse_athletic_1thumb.jpg");
+var fieldhouse_athletic_1 = new CarouselItem(["#fieldhouse"], fieldhouse_athletic_1full, fieldhouse_athletic_1thumb, "MFH Fitness Classes", 'Pedal on your own or take a spin class at the Mountaineer Field House Fitness Center. The MFH offers Rip, Spin, Athletic Conditioning, Yoga, Zumba and Core/Cardio/Balance classes. </br>&lt;a target="_self" onClick=&quot;window.location.href=&#x27; http://www.western.edu/fitness &#x27;&quot;href=" http://www.western.edu/fitness "&gt;Click here to read more...&lt;/a&gt;');
 
 
+var fieldhouse_athletic_2full = new Image("fieldhouse_athletic_2full", "imgs/fieldhouse_athletic_2full.jpg");
+var fieldhouse_athletic_2thumb = new Image("fieldhouse_athletic_2thumb", "imgs/fieldhouse_athletic_2thumb.jpg");
+var fieldhouse_athletic_2 = new CarouselItem(["#fieldhouse"], fieldhouse_athletic_2full, fieldhouse_athletic_2thumb, " MFH Climbing Wall",  'Climb 13 leadable routes, practice on two free-form cracks, learn multi-pitch skills, or try try your hand at climbing for the first time on Western’s 43.5 foot climbing wall.</br>&lt;a target="_self" onClick=&quot;window.location.href=&#x27;http://www.western.edu/climbingwall&#x27;&quot;   href="http://www.western.edu/climbingwall"&gt;Click here to read more...&lt;/a&gt;');
 
-var Taylor_MovieThumb = new Image("Taylor_MovieThumb", "imgs/Taylor_MovieThumb.png");
-var Taylor_Movie = new CarouselItem(["#taylor"], Taylor_MovieThumb, "Taylor Movie", '<a class="iframe" href="https://www.youtube.com/watch?v=NyYG2YRohp8"target="_blank" >');
+
+var fieldhouse_athletic_3full = new Image("fieldhouse_athletic_3full", "imgs/fieldhouse_athletic_3full.jpg");
+var fieldhouse_athletic_3thumb = new Image("fieldhouse_athletic_3thumb", "imgs/fieldhouse_athletic_3thumb.jpg");
+var fieldhouse_athletic_3 = new CarouselItem(["#fieldhouse"], fieldhouse_athletic_3full, fieldhouse_athletic_3thumb, 'MFH Fitness Center', 'The Mountaineer Field House Fitness Center is like your own personal gym. Utilize the facility’s free-weights or exercise machines. Or take advantage of the many fitness classes offered to students and community members.</br>&lt;a target="_self" onClick=&quot;window.location.href=&#x27;http://www.western.edu/mfh&#x27;&quot;   href="http://www.western.edu/mfh"&gt;Click here to read more...&lt;/a&gt;');
+
+var fieldhouse_athletic_4full = new Image("fieldhouse_athletic_4", "imgs/fieldhouse_athletic_4full.jpg");
+var fieldhouse_athletic_4thumb = new Image("fieldhouse_athletic_4thumb", "imgs/fieldhouse_athletic_4thumb.jpg");
+var fieldhouse_athletic_4 = new CarouselItem(["#fieldhouse"], fieldhouse_athletic_4full, fieldhouse_athletic_4thumb, "Highest Indoor Track", 'The Mountaineer Field House houses the highest indoor NCAA track in the country. The 200-meter, high-tech surface track allows runners and walkers to train throughout the cold months.</br>&lt;a target="_self" onClick=&quot;window.location.href=&#x27;http://www.western.edu/future-students/student-testimonials-and-profiles&#x27;&quot;   href="http://www.western.edu/future-students/student-testimonials-and-profiles"&gt;Click here to read more...&lt;/a&gt;');
+
+var fieldhouse_athletic_5full = new Image("fieldhouse_athletic_5full", "imgs/fieldhouse_athletic_5full.jpg");
+var fieldhouse_athletic_5thumb = new Image("fieldhouse_athletic_5thumb", "imgs/fieldhouse_athletic_5thumb.jpg");
+var fieldhouse_athletic_5 = new CarouselItem(["#fieldhouse"], fieldhouse_athletic_5full, fieldhouse_athletic_5thumb, "Courts, Practice Cages, Trampoline and more!", 'The track in the Mountaineer Field House wraps around three multi-purpose courts and a batting- and golf-practice cage. The facility also houses an in-ground trampoline and foam pit for freeski training, play and more.</br>&lt;a target="_self" onClick=&quot;window.location.href=&#x27;http://www.western.edu/mfh&#x27;&quot;   href="http://www.western.edu/mfh"&gt;Click here to read more...&lt;/a&gt;');
+
+//Fieldhouse___________________________________________________________
+
+var universitycenter_1full = new Image("universitycenter_2full", "imgs/universitycenter_1full.jpg");
+var universitycenter_1thumb = new Image("universitycenter_1thumb", "imgs/universitycenter_1thumb.jpg");
+var universitycenter_1 = new CarouselItem(["#universitycenter"], universitycenter_1full, universitycenter_1thumb, "The Bookstore", 'Find all things Western in the campus bookstore. Purchase your books for classes as well as school supplies, Western apparel and gifts, and books by local authors.<br /></br>&lt;a target="_self" onClick=&quot;window.location.href=&#x27;http://bookstore.western.edu &#x27;&quot;href="http://bookstore.western.edu"&gt;Click here to read more...&lt;/a&gt;');
+
+
+var universitycenter_2full = new Image("universitycenter_2full", "imgs/universitycenter_2full.jpg");
+var universitycenter_2thumb = new Image("universitycenter_2thumb", "imgs/universitycenter_2thumb.jpg");
+var universitycenter_2 = new CarouselItem(["#universitycenter"], universitycenter_2full, universitycenter_2thumb, "Great, Quick Food at Mad Jacks",  'Mad Jacks features a variety of snacks, beverages, Simply to Go options and Seattle’s Best coffee. Made-to-order options include Jamba Juice smoothies and Waldo’s Cheesie Grill, featuring gourmet grilled cheese sandwiches, specialty French fries, gooey melts and classic favorites.');
+
+
+var universitycenter_3full = new Image("universitycenter_3full", "imgs/universitycenter_3full.jpg");
+var universitycenter_3thumb = new Image("universitycenter_3thumb", "imgs/universitycenter_3thumb.jpg");
+var universitycenter_3 = new CarouselItem(["#universitycenter"], universitycenter_3full, universitycenter_3thumb, 'A Great Place to Hang Out', 'Challenge your friends to a game of pool on one of four tables in Western’s University Center lounge. The lounge also has a foosball table, and students can check out equipment for games at no cost.');
+
+var universitycenter_4full = new Image("universitycenter_4", "imgs/universitycenter_4full.jpg");
+var universitycenter_4thumb = new Image("universitycenter_4thumb", "imgs/universitycenter_4thumb.jpg");
+var universitycenter_4 = new CarouselItem(["#universitycenter"], universitycenter_4full, universitycenter_4thumb, " The Rare Air Café", 'Finding your favorite foods on campus is easy in Western’s dining hall, Rare Air Café. The wide variety of foods offered accommodate any food restrictions. Menus vary daily. Enjoy pizza and pasta, fresh fruit and salads, home-style entrees, deli options served, homemade soups, ice cream and desserts. All food is freshly prepared and served all-you-can-eat.');
+
+var universitycenter_5full = new Image("universitycenter_5full", "imgs/universitycenter_5full.jpg");
+var universitycenter_5thumb = new Image("universitycenter_5thumb", "imgs/universitycenter_5thumb.jpg");
+var universitycenter_5 = new CarouselItem(["#universitycenter"], universitycenter_5full, universitycenter_5thumb, "Amazing Dining Selections at Rare Air", 'Finding your favorite foods on campus is easy in Western’s dining hall, Rare Air Café. The wide variety of foods offered accommodate any food restrictions. Menus vary daily. Enjoy pizza and pasta, fresh fruit and salads, home-style entrees, deli options served, homemade soups, ice cream and desserts. All food is freshly prepared and served all-you-can-eat.');
+
+//var Taylor_MovieThumb = new Image("Taylor_MovieThumb", "imgs/Taylor_MovieThumb.png");
+//var Taylor_Movie = new CarouselItem(["#taylor"], Taylor_MovieThumb, "Taylor Movie", '<a class="iframe" href="https://www.youtube.com/watch?v=NyYG2YRohp8"target="_blank" >');
 
 
 //End of Carousel Items______________________________________________________________
 
 
-var CIs = [Taylor_WildPursuits, Taylor_STChrisNoah, Taylor_Movie, Taylor_STBradArcher,Taylor_STZoeSmith, Taylor_STBadArcher];
+var CIs = [taylor_1, taylor_2, taylor_3, taylor_4, taylor_5, fieldhouse_athletic_1, fieldhouse_athletic_2, fieldhouse_athletic_3, fieldhouse_athletic_4, fieldhouse_athletic_5, universitycenter_1, universitycenter_2,universitycenter_3, universitycenter_4, universitycenter_5];
 
